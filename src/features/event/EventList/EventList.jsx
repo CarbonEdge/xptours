29b10a2611a075551aea23d6b6ebb25fee4c59bf
CarbonEdge <wholetree65@gmail.com>
2018-08-18 20:22:37 +0200

    s5l41

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
-import EventListItem from './EventListItem'
+import React, { Component } from "react";
+import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
-    const {events} = this.props;
+    const { events, onEventOpen, deleteEvent } = this.props;
 
     return (
       <div>
         <h1>Event List</h1>
-        {events.map((event) => (
-          <EventListItem key={event.id} event={event} />
+        {events.map(event => (
+          <EventListItem
+            key={event.id}
+            event={event}
+            onEventEdit={onEventOpen}
+            deleteEvent ={deleteEvent}
+          />
         ))}
       </div>
     );
