29a38bd098f62454641946ad52ca5a507950df5a
CarbonEdge <wholetree65@gmail.com>
2018-08-27 17:16:58 +0200

    s8l71

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import { Container } from "semantic-ui-react";
-import { Route, Switch } from "react-router-dom";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
-import NavBar from "../../features/nav/NavBar/NavBar";
-import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
-import PeopleDashboard from "../../features/user/UserDetailed/UserDetailedPage";
-import UserDetailed from "../../features/user/UserDetailed/UserDetailedPage";
-import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
-import EventForm from "../../features/event/EventForm/EventForm";
-import HomePage from "../../features/home/HomePage";
-import TestComponent from "../../features/testarea/TestComponent";
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import EventForm from '../../features/event/EventForm/EventForm';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import HomePage from '../../features/home/HomePage';
+import TestComponent from '../../features/testarea/TestComponent';
 
 class App extends Component {
   render() {
@@ -18,6 +18,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
         </Switch>
+
         <Route
           path="/(.+)"
           render={() => (
@@ -27,11 +28,12 @@ class App extends Component {
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
                   <Route path="/test" component={TestComponent} />
-                  <Route path="/events:id" component={EventDetailedPage} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/manage/:id" component={EventForm} />
                   <Route path="/people" component={PeopleDashboard} />
-                  <Route path="/profile:id" component={UserDetailed} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
                   <Route path="/settings" component={SettingsDashboard} />
-                  <Route path="/creatEvents" component={EventForm} />
+                  <Route path="/createEvent" component={EventForm} />
                 </Switch>
               </Container>
             </div>
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,101 +1,33 @@
-import React, { Component } from "react";
-import { Grid, Button } from "semantic-ui-react";
-import EventList from "../EventList/EventList";
-import EventForm from "../EventForm/EventForm";
-import cuid from "cuid";
-import { connect } from "react-redux";
-import { createEvent, updateEvent, deleteEvent } from "../eventActions";
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { deleteEvent } from '../eventActions';
+import EventList from '../EventList/EventList';
 
 const mapState = state => ({
   events: state.events
 });
 
 const actions = {
-  createEvent,
-  updateEvent,
   deleteEvent
 };
+
 class EventDashboard extends Component {
-  state = {
-    isOpen: false,
-    selectedEvent: null
-  };
-
-  handleFormOpen = () => {
-    this.setState({
-      selectedEvent: null,
-      isOpen: true
-    });
-  };
-
-  handleCancel = () => {
-    this.setState({
-      isOpen: false
-    });
-  };
-
-  handleUpdateEvent = updatedEvent => {
-    this.props.updateEvent(updatedEvent);
-    this.setState({
-      isOpen: false,
-      selectedEvent: null
-    });
-  };
-
-  handleOpenEvent = eventToOpen => () => {
-    this.setState({
-      selectedEvent: eventToOpen,
-      isOpen: true
-    });
-  };
-
   handleDeleteEvent = eventId => () => {
     this.props.deleteEvent(eventId);
   };
 
-  handleCreateEvent = newEvent => {
-    newEvent.id = cuid();
-    newEvent.hostPhotoURL = "/assets/user.png";
-    this.props.createEvent(newEvent);
-    this.setState({
-      isOpen: false
-    });
-  };
-
   render() {
-    const { selectedEvent } = this.state;
     const { events } = this.props;
-
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList
-            deleteEvent={this.handleDeleteEvent}
-            onEventOpen={this.handleOpenEvent}
-            events={events}
-          />
+          <EventList deleteEvent={this.handleDeleteEvent} events={events} />
         </Grid.Column>
-        <Grid.Column width={6}>
-          <Button
-            onClick={this.handleFormOpen}
-            positive
-            content="Create Event"
-          />
-          {this.state.isOpen && (
-            <EventForm
-              updateEvent={this.handleUpdateEvent}
-              selectedEvent={selectedEvent}
-              createEvent={this.handleCreateEvent}
-              handleCancel={this.handleCancel}
-            />
-          )}
-        </Grid.Column>
+        <Grid.Column width={6} />
       </Grid>
     );
   }
 }
 
-export default connect(
-  mapState,
-  actions
-)(EventDashboard);
+export default connect(mapState, actions)(EventDashboard);
