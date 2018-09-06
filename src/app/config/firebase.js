5b24ad1a7f906a89af2926f5f298bd0623d02f66
CarbonEdge <wholetree65@gmail.com>
2018-09-06 22:31:19 +0200

    128

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-var config = {
-    apiKey: "AIzaSyBUas-lGb9QjsubJp7KoRqDzXel34OVgbo",
-    authDomain: "xptours123.firebaseapp.com",
-    databaseURL: "https://xptours123.firebaseio.com",
-    projectId: "xptours123",
-    storageBucket: "xptours123.appspot.com",
-    messagingSenderId: "824771565381"
+var firebaseConfig = {
+  apiKey: "AIzaSyBUas-lGb9QjsubJp7KoRqDzXel34OVgbo",
+  authDomain: "xptours123.firebaseapp.com",
+  databaseURL: "https://xptours123.firebaseio.com",
+  projectId: "xptours123",
+  storageBucket: "xptours123.appspot.com",
+  messagingSenderId: "824771565381"
   };
-  firebase.initializeApp(config);
-firebase.firestore();
-
-export default firebase;
+  
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore();
+  
+  export default firebase;
